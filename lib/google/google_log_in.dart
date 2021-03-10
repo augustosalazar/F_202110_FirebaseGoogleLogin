27037de0acacddb32b8407edde032e0a15ca307c
augustosalazar <augustosalazar@uninorte.edu.co>
2021-03-10 17:26:52 -0500

    basic login structure

---
+++ lib/google/google_log_in.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GoogleLogIn extends StatelessWidget {
+  const GoogleLogIn({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Log in")),
+    );
+  }
+}
